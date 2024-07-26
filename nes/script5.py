e5727e75ce1f83aada5a7dbd7709eae9daf0487b
htmlcoder2023 <nguyennamtran2022@gmail.com>
2024-07-26 14:12:47 -0500

    Update files

--- nes/script5.py
+++ nes/script5.py
@@ -3,6 +3,9 @@ import hashlib
 import zlib
 datFile = open("nes.dat", "r")
 lines = []
+linesArr = []
+sizes = []
+fileWrite = []
 while True:
     line = datFile.readline()
     if '<rom name="' and 'size="' in line:
@@ -21,6 +24,37 @@ while True:
         lines.append(line[6])
         lines.append(line[8])
         lines.append(line[10])
+        os.system("mkdir " + line[2])
+        linesArr.append(line[2])
+        sizes.append(open(line[2] + "/nes.dat", "w"))
+        fileWrite.append(0)
     if not line:
         break
+
+while True:
+    line = datFile.readline()
+    if '<rom name="' and 'size="' in line:
+        line = line.replace("		", "")
+        line = line.replace('<rom name="', "")
+        line = line.split('"')
+        for arrs in range(len(linesArr)):
+            if line[2] == linesArr[arrs]:
+                if fileWrite[arrs] == 0:
+                    sizes[arrs].write(lines[0])
+                    sizes[arrs].write(lines[2])
+                    sizes[arrs].write(lines[4])
+                    sizes[arrs].write(lines[6])
+                    sizes[arrs].write(lines[8])
+                    sizes[arrs].write(lines[10])
+                else:
+                    sizes[arrs].write("\n" + lines[0])
+                    sizes[arrs].write("\n" + lines[2])
+                    sizes[arrs].write("\n" + lines[4])
+                    sizes[arrs].write("\n" + lines[6])
+                    sizes[arrs].write("\n" + lines[8])
+                    sizes[arrs].write("\n" + lines[10])
+                fileWrite[arrs] += 1
+    if not line:
+        break
+
 datFile.close()
