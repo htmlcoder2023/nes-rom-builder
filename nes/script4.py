aba09d0102a973ca170e893d239d9bfd1bc6638e
htmlcoder2023 <nguyennamtran2022@gmail.com>
2024-07-27 20:38:58 -0400

    Update files

--- nes/script4.py
+++ nes/script4.py
@@ -2,6 +2,15 @@ import urllib.request
 import os
 from zipfile import ZipFile
 import platform
+from urllib.parse import urlparse
+
+def uri_validator(x):
+    try:
+        result = urlparse(x)
+        return all([result.scheme, result.netloc])
+    except AttributeError:
+        return False
+
 datFile = open("nes.dat", "r")
 games = []
 while True:
@@ -11,36 +20,21 @@ while True:
     if not line:
         break
 
-datFile = open("games.dat", "w")
-lines = 0
-if platform.system() == "Windows":
-    os.system("del *.nes")
-    os.system("del *.zip")
-else:
-    os.system("chmod +w *.nes")
-    os.system("chmod +w *.zip")
-    os.system("rm *.nes")
-    os.system("rm *.zip")
-while lines < len(games):
-    games[lines] = games[lines].replace("amp;", "")
-    games[lines] = games[lines].replace(".unh", ".nes")
-    datFile.write(games[lines])
-    lines += 1
-datFile.close()
-
 lines = 0
 
 for lines in range(len(games)):
-    if os.path.isfile(games[lines]):
+    if os.path.isfile(games[lines]) or not uri_validator("https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Nintendo%20Entertainment%20System%20%28Headered%29/" + games[lines]):
         games.pop(lines)
+        lines -= 1
         continue
     else:
+        games[lines] = games[lines].replace("amp;", "")
         games[lines] = games[lines].replace("(", "%28")
         games[lines] = games[lines].replace("\n", "")
         games[lines] = games[lines].replace(")", "%29")
         games[lines] = games[lines].replace(" ", "%20")
         games[lines] = games[lines].replace(".bin", ".zip")
-        games[lines] = games[lines].replace(".nes", ".zip")
+        games[lines] = games[lines].replace(".unh", ".zip")
         games[lines] = games[lines].replace(",", "%2C")
         games[lines] = games[lines].replace("[", "%5B")
         games[lines] = games[lines].replace("]", "%5D")
@@ -66,8 +60,17 @@ while True:
         print(str(lines + 1) + " files extracted!")
     break
 
+datFile = open("games.dat", "w")
+lines = 0
 if platform.system() == "Windows":
     os.system("del *.zip")
 else:
     os.system("chmod +w *.zip")
     os.system("rm *.zip")
+while lines < len(games):
+    if lines == 0:
+        datFile.write(games[lines])
+    else:
+        datFile.write("\n" + games[lines])
+    lines += 1
+datFile.close()
