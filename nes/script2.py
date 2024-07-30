3bd0c24e61367de2577c51c0e4cb243aece0e010
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-07-30 14:49:07 -0400

    Update script2.py

--- nes/script2.py
+++ nes/script2.py
@@ -51,9 +51,9 @@ if mode == "PRG":
     for files in range(len(roms)):
         romCount = 0
         roms[files] = roms[files].replace(",", " and")
-        os.system('ren "' + roms[files].replace(" and", ",") + '" ' + roms[files])
+        os.system('ren "' + roms[files].replace(" and", ",") + '" "' + roms[files] + '"')
         roms[files] = roms[files].replace("&", "and")
-        os.system('ren "' + roms[files].replace("and", "&") + '" ' + roms[files])
+        os.system('ren "' + roms[files].replace("and", "&") + '" "' + roms[files] + '"')
         os.system("mkdir " + roms[files].replace(" ", "-"))
         os.system('nessplitter "' + roms[files] + '" ' + roms[files].replace(" ", "-"))
         if romCount < 10:
@@ -112,9 +112,9 @@ else:
     for files in range(len(roms)):
         romCount = 0
         roms[files] = roms[files].replace(",", " and")
-        os.system('ren "' + roms[files].replace(" and", ",") + '" ' + roms[files])
+        os.system('ren "' + roms[files].replace(" and", ",") + '" "' + roms[files] + '"')
         roms[files] = roms[files].replace("&", "and")
-        os.system('ren "' + roms[files].replace("and", "&") + '" ' + roms[files])
+        os.system('ren "' + roms[files].replace("and", "&") + '" "' + roms[files] + '"')
         os.system("mkdir " + roms[files].replace(" ", "-"))
         os.system('nessplitter "' + roms[files] + '" ' + roms[files].replace(" ", "-"))
         if romCount < 10:
