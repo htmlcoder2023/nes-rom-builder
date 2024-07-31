ec2c24ed4531a9fc788b13e8d6d27bca981ad1dc
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-07-31 16:24:38 -0400

    Update script4.py

--- fds/script4.py
+++ fds/script4.py
@@ -1,9 +1,10 @@
-import urllib.request
 import os
 from zipfile import ZipFile
 import platform
-datFile = open("fds.dat", "r")
+
+datFile = open("fds_roms.dat", "r")
 games = []
+
 while True:
     line = datFile.readline()
     if "(" in line:
@@ -11,63 +12,39 @@ while True:
     if not line:
         break
 
-datFile = open("games.dat", "w")
-lines = 0
-
-if platform.system() == "Windows":
-    os.system("del *.fds")
-    os.system("del *.bin")
-    os.system("del *.zip")
-else:
-    os.system("chmod +w *.fds")
-    os.system("chmod +w *.bin")
-    os.system("chmod +w *.zip")
-    os.system("rm *.fds")
-    os.system("rm *.bin")
-    os.system("rm *.zip")
-
-while lines < len(games):
-    games[lines] = games[lines].replace("amp;", "")
-    datFile.write(games[lines])
-    lines += 1
 datFile.close()
 
 lines = 0
+datFile = open("games.dat", "w")
+
+with ZipFile("../fds/fds-roms.zip", 'r') as zObject: 
+    zObject.extractall(
+        path="../fds"
+    )
+
+writtenNum = 0
 
 for lines in range(len(games)):
-    games[lines] = games[lines].replace("(", "%28")
     games[lines] = games[lines].replace("\n", "")
-    games[lines] = games[lines].replace(")", "%29")
-    games[lines] = games[lines].replace(" ", "%20")
+    games[lines] = games[lines].replace("amp;", "")
     games[lines] = games[lines].replace(".bin", ".zip")
-    games[lines] = games[lines].replace(".fds", ".zip")
-    games[lines] = games[lines].replace(",", "%2C")
-    games[lines] = games[lines].replace("[", "%5B")
-    games[lines] = games[lines].replace("]", "%5D")
-    games[lines] = games[lines].replace("&", "%26")
-    games[lines] = games[lines].replace("+", "%2B")
+    if os.path.isfile(games[lines]):
+        games.pop(lines)
+        lines -= 1
+        continue
+    else:
+        try:
+            with ZipFile("../fds/" + games[lines].replace(".fds", ".zip"), 'r') as zObject:
+                zObject.extractall(
+                    path="../fds"
+                )
+        except:
+            pass
 
-while True:
-    for lines in range(len(games)):
-        while True:
-            try:
-                urllib.request.urlretrieve("https://myrient.erista.me/files/No-Intro/Nintendo%20-%20Family%20Computer%20Disk%20System%20%28FDS%29/" + games[lines], games[lines])
-            except:
-                print("Failed to download " + games[lines] + "!")
-                pass
-            try:
-                with ZipFile("../fds/" + games[lines], 'r') as zObject: 
-                    zObject.extractall(
-                        path="../fds"
-                    )
-                break
-            except:
-                pass
-        print(str(lines + 1) + " files extracted!")
-    break
+    if writtenNum == 0:
+        datFile.write(games[lines])
+    else:
+        datFile.write("\n" + games[lines])
+    writtenNum += 1
 
-if platform.system() == "Windows":
-    os.system("del *.zip")
-else:
-    os.system("chmod +w *.zip")
-    os.system("rm *.zip")
+datFile.close()
