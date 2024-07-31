a76a1ac8bf1a92c47736ae72641c2a72e70e0392
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-07-31 17:40:20 -0400

    Update script4.py

--- script4.py
+++ script4.py
@@ -3,6 +3,8 @@ from zipfile import ZipFile
 import platform
 
 romZip = input("What .zip file contains the zipped ROMs? ")
+if os.path.isfile(romZip) != True:
+    raise Exception("File not found!")
 inputFile = input("What file contains the ROM hashes? ")
 if os.path.isfile(inputFile):
     datFile = open(inputFile, "r")
