e989fbf469b4b0324f68a682fe78ccc438f60a61
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2024-07-31 22:24:29 -0400

    Update script4.py

--- script4.py
+++ script4.py
@@ -42,11 +42,12 @@ with ZipFile("../" + inputDir + "/" + romZip, 'r') as zObject:
 writtenNum = 0
 
 fileExtension_num = int(input("How many file extensions need to be replaced? "))
+fileNum = 0
 
 for lines in range(len(games)):
     games[lines] = games[lines].replace("\n", "")
     games[lines] = games[lines].replace("amp;", "")
-    for thugs in range(fileExtension_num):
+    while fileNum < fileExtension_num:
         fileExtension = input("Which file extension? ")
         games[lines] = games[lines].replace(fileExtension, ".zip")
     if os.path.isfile(games[lines]):
